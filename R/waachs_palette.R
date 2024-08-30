807c104079d303767d8663a71322c48627a69880
dempz <69284007+dempz@users.noreply.github.com>
2024-08-30 09:29:05 +0800

    build package
    update documentation

--- R/waachs_palette.R
+++ R/waachs_palette.R
@@ -10,7 +10,9 @@
 #' @return Vector with colour hex codes. If `type == "continuous"` returns vector of length `n`. If `visualisation == TRUE` returns a list containing colour palette vector and `ggplot2` visualisation of colour spectrum.
 #'
 #' @examples
-#' colours <- waachs_palette(type = "continuous", n = 100, visualisation = TRUE) # Render colour palette
+#' colours <- waachs_palette(type = "continuous",
+#'                           n = 100,
+#'                           visualisation = TRUE) # Render colour palette
 #' print(colours$palette) # Print colour palette
 #' print(colours$plot)    # Print plot
 #'
@@ -22,7 +24,10 @@ waachs_palette <- function(type = "discrete",
                            bias = 2,
                            interpolate = "spline",
                            ...){
-  waachs_palette <- c("#D58957", "#FCD16B", "#89A1AD", "#FEF0D8")
+  waachs_palette <- c("#D58957",
+                      "#FCD16B",
+                      "#89A1AD",
+                      "#FEF0D8")
 
   if (type == "discrete"){
     palette <- waachs_palette
@@ -38,12 +43,17 @@ waachs_palette <- function(type = "discrete",
 
   if (visualisation == TRUE){
 
-    viz_dat <- data.frame(x = rep(1:ceiling(sqrt(n)), each = ceiling(sqrt(n)))[1:n],
-                          y = rep(1:ceiling(sqrt(n)), times = ceiling(sqrt(n)))[1:n],
+    viz_dat <- data.frame(x = rep(1:ceiling(sqrt(n)),
+                                  each = ceiling(sqrt(n)))[1:n],
+                          y = rep(1:ceiling(sqrt(n)),
+                                  times = ceiling(sqrt(n)))[1:n],
                           colours = palette
                           )
 
-    plot <- ggplot2::ggplot(viz_dat, aes(x = x, y = y, fill = colours)) +
+    plot <- ggplot2::ggplot(viz_dat,
+                            aes(x = x,
+                                y = y,
+                                fill = colours)) +
       ggplot2::geom_tile() +
       ggplot2::scale_fill_identity() +
       ggplot2::theme_void() +
