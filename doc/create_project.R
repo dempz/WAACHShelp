e4ec9c42b4566886cec5b4d477b1590bdffb7885
dempz <69284007+dempz@users.noreply.github.com>
2024-11-07 12:32:16 +0800

    Re-try building vignettes

---
+++ doc/create_project.R
@@ -0,0 +1,18 @@
+## ----include = FALSE, message=F, warning=F------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, message=F, warning=F, error = F-----------------------------------
+library(WAACHShelp) # Let's load the WAACHShelp package
+
+## ----eval = F-----------------------------------------------------------------
+#  create_project(project_name = "test_project")
+
+## ----echo=FALSE, out.width="95%"----------------------------------------------
+knitr::include_graphics("images/create_project/2_prompt.png")
+
+## ----echo=FALSE, out.width="95%"----------------------------------------------
+knitr::include_graphics("images/create_project/3_project.png")
+
