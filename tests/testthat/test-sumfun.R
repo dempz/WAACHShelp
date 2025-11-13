3489a3f4e538cb19391aa0e67ac1f0d0beee7b78
dempz <69284007+dempz@users.noreply.github.com>
2025-11-13 15:08:07 +1100

    Deprecate warning sumfun

--- tests/testthat/test-sumfun.R
+++ tests/testthat/test-sumfun.R
@@ -1,6 +1,6 @@
 test_that("sumfun returns a data.frame with correct columns", {
   x <- c(1, 2, 3, 4, 5)
-  res <- sumfun(x)
+  res <- suppressWarnings(sumfun(x))
 
   expect_s3_class(res, "data.frame")
   expect_named(res, c("n", "miss", "mean", "sd", "med", "q25", "q75", "min", "max"))
@@ -8,7 +8,7 @@ test_that("sumfun returns a data.frame with correct columns", {
 
 test_that("sumfun calculates correct basic statistics", {
   x <- c(1, 2, 3, 4, 5)
-  res <- sumfun(x)
+  res <- suppressWarnings(sumfun(x))
 
   expect_equal(res$n, length(x))
   expect_equal(res$miss, 0)
@@ -23,7 +23,7 @@ test_that("sumfun calculates correct basic statistics", {
 
 test_that("sumfun handles missing values correctly with na.rm = TRUE", {
   x <- c(1, 2, NA, 4)
-  res <- sumfun(x)
+  res <- suppressWarnings(sumfun(x))
 
   expect_equal(res$miss, 1)
   expect_equal(res$n, length(x))
@@ -32,5 +32,9 @@ test_that("sumfun handles missing values correctly with na.rm = TRUE", {
 
 test_that("sumfun errors with NAs if na.rm = FALSE", {
   x <- c(1, 2, NA, 4)
-  expect_error(sumfun(x, na.rm = FALSE), "missing values and NaN's not allowed")
+  expect_error(suppressWarnings(sumfun(x, na.rm = FALSE)), "missing values and NaN's not allowed")
+})
+
+test_that("sumfun is deprecated", {
+  lifecycle::expect_deprecated(sumfun(c(1, 2, 3, 4)))
 })
